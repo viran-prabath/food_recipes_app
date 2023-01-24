5372ce3604dfdb41b2df13e24e8b06f9fb05dc72
vpjproduction <viranprabath1996@gmail.com>
2023-01-24 10:55:25 -0800

    Initial Commit

---
+++ food_recipes_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  food_recipes_app
+//
+//  Created by VPJ Production on 1/24/23.
+//  Copyright © 2023 VPJ Production. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
